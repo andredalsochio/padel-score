9f400697d780dbd49ed8ec87fb9d26b5ac8170a3
Andre Dalsochio <andrefdalsochio@gmail.com>
2025-11-05 20:20:10 -0300

    refactor(home): extract Shimmer widget using HomeGradients and modern alpha API

---
+++ lib/features/home/widgets/shimmer.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../helpers/home_styles.dart';
+
+class Shimmer extends StatefulWidget {
+  const Shimmer({super.key, required this.child});
+  final Widget child;
+
+  @override
+  State<Shimmer> createState() => _ShimmerState();
+}
+
+class _ShimmerState extends State<Shimmer> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2))
+      ..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        final t = _controller.value;
+        return DecoratedBox(
+          decoration: BoxDecoration(
+            gradient: HomeGradients.shimmer(t, scheme.surface, scheme.surfaceVariant),
+          ),
+          child: child,
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
